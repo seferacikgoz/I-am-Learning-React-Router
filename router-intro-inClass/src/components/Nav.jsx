7a21799f825f4841077ec7bc44d0633b3be29c43
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-31 14:58:33 +0300

    Link to

--- router-intro-inClass/src/components/Nav.jsx
+++ router-intro-inClass/src/components/Nav.jsx
@@ -4,13 +4,13 @@ const Nav = () => {
   return (
     <ul>
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/instructors">Instructors</a>
+        <Link to="/instructors">Instructors</Link>
       </li>
       <li>
-        <a href="/contact">Contact</a>
+        <NavLink to="/contact">Contact</NavLink>
       </li>
     </ul>
   );
