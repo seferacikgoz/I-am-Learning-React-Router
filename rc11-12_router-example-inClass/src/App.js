b8f16489f5f8a1133c1eb97b820be5c26ee58015
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-01 00:54:22 +0300

    v1

--- rc11-12_router-example-inClass/src/App.js
+++ rc11-12_router-example-inClass/src/App.js
@@ -5,12 +5,6 @@ import Home from "./pages/Home";
 import People from "./pages/People";
 import NotFound from "./pages/NotFound";
 import { Routes, Route, Link, Navigate } from "react-router-dom";
-import Paths from "./pages/Paths";
-import PersonDetail from "./pages/PersonDetail";
-import FullStack from "./pages/FullStack";
-import Aws from "./pages/Aws";
-import PrivateRouter from "./pages/PrivateRouter";
-import Login from "./pages/Login";
 
 //? Link, NavLink ve Navigate componentleri declerative routing
 //? gerceklestirmek icin kullanilir.
@@ -30,29 +24,10 @@ function App() {
   return (
     <>
       <Nav />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="people" element={<People />} />
-        <Route path="people/:id" element={<PersonDetail />} />
-
-        {/* nested route */}
-        <Route path="paths" element={<Paths />}>
-          <Route index element={<FullStack />} />
-          {/* <Route path="fullstack" element={<FullStack />} /> */}
-          <Route path="aws" element={<Aws />} />
-        </Route>
-
-        <Route path="contact" element={<PrivateRouter />}>
-          <Route path="" element={<Contact />} />
-        </Route>
-
-        <Route path="login" element={<Login />} />
-
-        {/* <Route path="*" element={<NotFound />} /> */}
-
-        {/* Redirect islemleri icin Navigate componenti kullanilabilir */}
-        {/* <Route path="*" element={<Navigate to="/" />} /> */}
-      </Routes>
+      <Home/>
+      <People/>
+      <Contact/>
+      <NotFound/>    
       <Footer />
     </>
   );
