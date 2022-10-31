0640f0c7aa91e430712478ccb5d6890b91d8f791
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-31 13:23:48 +0300

    I am Learning React Router

---
+++ router-intro-inClass/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import { Link, NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/instructors">Instructors</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Nav;
