403b35626c9d7663c4c9fa4b034145b86f46d908
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-01 23:26:01 +0300

    btn_onclick

--- rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,6 +1,7 @@
-import { useParams, useLocation } from "react-router-dom"
+import { useParams, useLocation, useNavigate } from "react-router-dom"
 
 const PersonDetail = () => {
+  const navigate = useNavigate()
   const { id } = useParams()
   console.log(id);
 
@@ -13,10 +14,10 @@ const PersonDetail = () => {
       <img className="rounded" src={person.avatar} alt="" />
       <p>{person.email}</p>
       <div>
-        <button className="btn btn-success me-2">Go 
+        <button onClick={() => navigate("/")} className="btn btn-success me-2">Go 
           Home
         </button>
-        <button className="btn btn-warning">
+        <button onClick={() => navigate(-1)} className="btn btn-warning">
           Go Back
         </button>
       </div>
