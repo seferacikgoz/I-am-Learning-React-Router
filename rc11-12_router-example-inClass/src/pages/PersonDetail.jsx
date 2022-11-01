5e8816267749ff3a35ce8a1abcb240ade71bb2aa
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-02 01:56:52 +0300

    optional chaining

--- rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -21,9 +21,9 @@ const PersonDetail =  () => {
 
   return (
     <div className="container text-center">
-      <h3>{person.first_name}{person.last_name}</h3>
-      <img className="rounded" src={person.avatar} alt="" />
-      <p>{person.email}</p>
+      <h3>{person?.first_name}{person?.last_name}</h3>
+      <img className="rounded" src={person?.avatar} alt="" />
+      <p>{person?.email}</p>
       <div>
         <button onClick={() => navigate("/")} className="btn btn-success me-2">Go 
           Home
