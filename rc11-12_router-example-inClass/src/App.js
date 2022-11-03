14f2b0e42eccb5d207b121d9ef160f16d34cdfeb
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-03 16:45:51 +0300

    contact

--- rc11-12_router-example-inClass/src/App.js
+++ rc11-12_router-example-inClass/src/App.js
@@ -9,6 +9,8 @@ import Paths from "./pages/Paths";
 import PersonDetail from "./pages/PersonDetail";
 import FullStack from "./pages/FullStack"
 import Aws from "./pages/Aws"
+import PrivateRouter from "./pages/PrivateRouter";
+import Login from "./pages/Login";
 
 //? Link, NavLink ve Navigate componentleri declerative routing
 //? gerceklestirmek icin kullanilir.
@@ -40,8 +42,12 @@ function App() {
 
      </Route>
 
+    <Route path="contact" element={<PrivateRouter/>}>
+    <Route path="" element={<Contact/>}/>
+    </Route>
+  
+    <Route path="login" element={<Login/>}/>
 
-     <Route path="/contact" element={<Contact/>}/>
      {/* <Route path="/*" element={<NotFound/>}/> */}
      {/* <Route path="*" element={<Navigate to="/"/>}/> */} 
 </Routes>
--- rc11-12_router-example-inClass/src/pages/PrivateRouter.jsx
+++ rc11-12_router-example-inClass/src/pages/PrivateRouter.jsx
@@ -1,9 +1,13 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom"
+
 
 const PrivateRouter = () => {
-  const user = false;
+  const user = true
 
-  return <div>{user ? <Outlet /> : <Navigate to="/login" />}</div>;
-};
+  return   <div>
+    {user ? <Outlet/> : <Navigate to = "login" />}
+  </div>
+  
+}
 
-export default PrivateRouter;
+export default PrivateRouter
